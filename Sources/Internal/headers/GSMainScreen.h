0e322f4cc9b344dab21793cfce979bbc9a643cbb
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2021-11-16 16:41:47 +0300

    no message

--- Sources/Internal/headers/GSMainScreen.h
+++ Sources/Internal/headers/GSMainScreen.h
@@ -1,8 +1,6 @@
 #pragma once
 
-CG_EXTERN CGRect *CGSNextRect(CGSRegionEnumeratorRef enumerator);
-
-CG_EXTERN CGRect GSFullScreenApplicationContentRect()
+CG_EXTERN CGRect GSFullScreenApplicationContentRect();
 CG_EXTERN CGSize GSMainScreenSize();
 
 typedef struct __GSWindow *GSWindowRef;
