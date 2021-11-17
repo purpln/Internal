39cd5a9bc4d93332921968bcc1199164daa6a0b2
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2021-11-17 17:29:03 +0300

    no message

--- Sources/Internal/Internal.h
+++ Sources/Internal/Internal.h
@@ -3,6 +3,7 @@
 #ifdef __APPLE__
     #include <CoreGraphics/CoreGraphics.h>
 
+    #include "headers/GSEvent.h"
     #include "headers/SpringBoardServices.h"
     #include "headers/GSMainScreen.h"
 #endif
